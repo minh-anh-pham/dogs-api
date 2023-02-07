d60c52f96e0db457a7b66f526a73783f4547a0c6
minh-anh-pham <102440587+minh-anh-pham@users.noreply.github.com>
2023-02-07 13:13:54 +0000

    Write tests for POST & DELETE endpoints

--- index.test.js
+++ index.test.js
@@ -33,4 +33,37 @@ describe('Endpoints', () => {
             expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
         });
     });
+
+    describe('POST /dogs', () => {
+        it('should post a dog\'s data', async () => {
+            // make a request
+            const response = await request(app)
+                .post('/dogs')
+                .send(testDogData);
+            // assert a response code
+            expect(response.status).toBe(200);
+            // expect a response
+            expect(response.body).toBeDefined();
+            // toEqual checks deep equality in objects
+            expect(response.body[0]).toEqual(testDogData);
+        });
+
+        it('should check id of dog', async() => {
+            const response = await request(app)
+                .post('/dogs')
+                .send(testDogData);
+            const dog = Dog.findAll({where: id = response.body.id});
+            expect(response.body).toEqual(dog);
+        });
+    });
+
+    describe('DELETE /dogs/:id', () => {
+        it('should return list of dogs with correct data', async () => {
+            // make a request
+            const response = await request(app).delete('/dogs/1');
+            const dog = Dog.findAll({where: id = response.body.id});
+            // expect a null response
+            expect(dog).toBeNull();
+        });
+    });
 });
